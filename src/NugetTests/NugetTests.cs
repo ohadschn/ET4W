3bac648cfaf9b62a01ccd80d6fce8ebb7c72b196
ohads <ohads@microsoft.com>
2016-10-17 01:53:24 +0300

    NuGet tests

---
+++ src/NugetTests/NugetTests.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace NugetTests
+{
+    [TestClass]
+    public class NugetTests
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            new NugetEvents().Foo();
+        }
+    }
+}
